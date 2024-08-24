3bd887bc66f627f0a92f4e4a182b352a13236221
Lewiscr7 <134457825+Lewiscr7@users.noreply.github.com>
2024-08-23 21:55:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        const novaTarefa = '<li><span class="tarefa">' + $('#form-tarefa input[type="text"]').val() + '</span><div class="botao-concluida"></div></li>';            
+        $('ul').append(novaTarefa);
+            $('#nova-tarefa').val('');
+
+            $('ul li.tarefa').on('click', function() {
+                $(this).parent().toggleClass('botao-concluida');
+                $(this).toggleClass('line-through');
+            });
+
+            $('ul li').click( function(){
+                $(this).css("text-decoration","line-through");
+            });
+        });
+
+        
+    });
